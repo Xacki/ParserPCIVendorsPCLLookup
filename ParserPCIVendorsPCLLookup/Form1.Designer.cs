8ca47645c0ec31b5dcc55ad67644ea95a605f9d4
Raevsky Ra <kaneky.ky@outlook.com>
2022-09-12 15:19:01 +0300

    1.0

---
+++ ParserPCIVendorsPCLLookup/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ParserPCIVendorsPCLLookup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Info from devicehunt.com";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(401, 238);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(419, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(488, 238);
+            this.listBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(419, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Create Info from pcilookup.com";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(189, 259);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(224, 26);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(600, 256);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(307, 29);
+            this.progressBar2.TabIndex = 3;
+            this.progressBar2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 294);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 26);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Save to File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(419, 294);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 26);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Save to File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 450);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ParserPCIVendorsPCLLookup/Form1.cs
@@ -0,0 +1,106 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParserPCIVendorsPCLLookup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class JsonObject
+        {
+            public string desc { get; set; }
+            public string id { get; set; }
+            public string venDesc { get; set; }
+            public string venID { get; set; }
+
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string qurySelectCell;
+            IHtmlCollection<IElement> htmlElement;
+
+            IConfiguration config = Configuration.Default.WithDefaultLoader();
+            string address = "https://devicehunt.com/all-pci-vendors";
+            IBrowsingContext context = BrowsingContext.New(config);
+            IDocument document = await context.OpenAsync(address);
+
+            qurySelectCell = "tr td:nth-child(1)";
+            htmlElement = document.QuerySelectorAll(qurySelectCell);
+
+            IEnumerable<string> vendorId = htmlElement.Select(m => m.TextContent);
+
+            qurySelectCell = "tr td:nth-child(2)";
+            htmlElement = document.QuerySelectorAll(qurySelectCell);
+
+            IEnumerable<string> vendorName = htmlElement.Select(m => m.TextContent);
+
+            var numbersAndWords = vendorId.Zip(vendorName, (first, second) => first.ToUpper()+" "+ second);
+
+            progressBar1.Maximum = numbersAndWords.Count();
+            progressBar1.Visible = true;
+
+            foreach (var str in numbersAndWords)
+            {
+                listBox1.Items.Add(str);
+                progressBar1.Value++;
+            }
+            progressBar1.Visible = false;
+
+        }
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string line = "";
+
+             using (WebClient wc  = new WebClient())
+                line = wc.DownloadString("https://www.pcilookup.com/api.php?action=search&vendor=&device=&_=1662980845774");
+
+            List<JsonObject> json = JsonConvert.DeserializeObject<List<JsonObject>>(line);
+            progressBar2.Maximum = json.Count;
+            progressBar2.Visible = true;
+
+            foreach (JsonObject val in json)
+            {
+                listBox2.Items.Add($"{val.venID} {val.venDesc} {val.id} {val.desc}");
+                progressBar2.Value++;
+            }
+            progressBar2.Visible = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("C:\\1.txt"))
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                    sw.WriteLine(listBox1.Items[i].ToString());
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("C:\\2.txt"))
+            {
+                for (int i = 0; i < listBox2.Items.Count; i++)
+                    sw.WriteLine(listBox2.Items[i].ToString());
+            }
+        }
+    }
+}
